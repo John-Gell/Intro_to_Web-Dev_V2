aff8a18698a79c60f1ebdb287b5b9069a2c90d77
Your Name <you@example.com>
2020-05-07 10:09:39 -0400

    changed port on server from local to process.env.PORT

--- P06-Node_Project/server.js
+++ P06-Node_Project/server.js
@@ -54,5 +54,6 @@ app.get("/insult", function(req, res) {
 
 app.use("/public", express.static("./public"));
 
-app.listen(3000);
+const port = process.env.PORT || 3000;
+app.listen(port);
 console.log("listening on http://localhost:3000");
